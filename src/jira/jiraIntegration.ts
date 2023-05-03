4b4cc202eb5c5ece383caf7604fd58edff38c099
snx23d <antotalgin@gmail.com>
2023-05-03 18:51:03 +0200

    adds error handling for cloned code

--- src/jira/jiraIntegration.ts
+++ src/jira/jiraIntegration.ts
@@ -3,8 +3,16 @@ import { spec } from "pactum";
 import { JiraConfig, JiraHook } from "./interfaces";
 
 const jiraConfigContent = readFileSync("./jira.json", "utf-8");
-const jiraHooksContent = readFileSync("./jiraHooks.json", "utf-8");
 const mappingContent = readFileSync("./src/jira/testMapping.json", "utf-8");
+
+let jiraHooksContent: string;
+let isConfigPresent = true;
+try {
+    jiraHooksContent = readFileSync("./jiraHooks.json", "utf-8");
+} catch (error) {
+    jiraHooksContent = '{}';
+    isConfigPresent = false;
+}
 
 const jiraConfig: JiraConfig = JSON.parse(jiraConfigContent);
 const jiraHooks: JiraHook = JSON.parse(jiraHooksContent);
@@ -16,11 +24,10 @@ function mapTestNames(testNames: string[]) {
         .filter(v => undefined !== v);
 }
 
-
-export async function postFailedTests(testNames: string[]) {
-    const jiraTickets = mapTestNames(testNames);
-    const url = jiraConfig.baseUrl.concat(jiraHooks.testFailed);
-
+async function sendJiraRequest(
+    url: string,
+    jiraTickets: string[]
+) {
     await spec()
         .post(url)
         .withHeaders('Content-Type', 'application/json')
@@ -29,20 +36,33 @@ export async function postFailedTests(testNames: string[]) {
         })
         .expectStatus(200)
         .expectBody(null);
+}
+
+
+export async function postFailedTests(testNames: string[]) {
+    const jiraTickets = mapTestNames(testNames);
+
+    if (isConfigPresent) {
+        const url = jiraConfig.baseUrl.concat(jiraHooks.testFailed);
+        await sendJiraRequest(url, jiraTickets);
+
+    } else {
+        console.log(`pretend to move to backlog the following tickets:\n${jiraTickets.join("\n")}\n`);
+
+    }
 
 }
 
 export async function postPassedTests(testNames: string[]) {
     const jiraTickets = mapTestNames(testNames);
-    const url = jiraConfig.baseUrl.concat(jiraHooks.testPassed);
 
-    await spec()
-        .post(url)
-        .withHeaders('Content-Type', 'application/json')
-        .withBody({
-            issues: jiraTickets
-        })
-        .expectStatus(200)
-        .expectBody(null);
+    if (isConfigPresent) {
+        const url = jiraConfig.baseUrl.concat(jiraHooks.testPassed);
+        await sendJiraRequest(url, jiraTickets);
+
+    } else {
+        console.log(`pretend to change the status to DONE for the following tickets:\n${jiraTickets.join("\n")}\n`);
+
+    }
 
 }
